281899bff5f161c60933827dd6673b4090653862
Edson Kropniczki <edsonkrop@yahoo.com>
2020-02-18 11:08:59 -0300

    Included unversioned files (gradle, etc)

--- app/src/main/java/br/com/pearls/Settings/LanguageMultiSelect.java
+++ app/src/main/java/br/com/pearls/Settings/LanguageMultiSelect.java
@@ -1,14 +1,27 @@
 package br.com.pearls.Settings;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.preference.MultiSelectListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceViewHolder;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import br.com.pearls.R;
 
 public class LanguageMultiSelect extends MultiSelectListPreference {
 
@@ -17,10 +30,13 @@ public class LanguageMultiSelect extends MultiSelectListPreference {
     public LanguageMultiSelect(Context context, AttributeSet attributes) {
         super(context, attributes);
 
+        // get db language entries from SettingsActivity
         Bundle bundle = SettingsActivity.getLanguageEntries();
-        this.setEntries(bundle.getCharSequenceArray("entries"));
+
+        // set entries, entry values and default values
+        setEntries(bundle.getCharSequenceArray("entries"));
         CharSequence[] entryValues = bundle.getCharSequenceArray("entryValues");
-        this.setEntryValues(entryValues);
+        setEntryValues(entryValues);
 //        for(int ix = 0; ix < entryValues.length; ix++) {
 //            Log.v(TAG, "xxxxxxxxxxxxx ix = " + ix + "   ->    " + entryValues[ix]);
 //        }
@@ -34,6 +50,67 @@ public class LanguageMultiSelect extends MultiSelectListPreference {
         } else {
             setDefaultValue("");
         }
+
+        // watch if user left at least one language option selected
+        setOnPreferenceChangeListener(languageChangeListener);
+
+//        setOnPreferenceClickListener(new OnPreferenceClickListener() {
+//            @Override
+//            public boolean onPreferenceClick(Preference preference) {
+//
+//
+//                TextView textView = new TextView(context);
+//                textView.setText("At least one language checked");
+//                final AlertDialog.Builder d = new AlertDialog.Builder(context);
+//                d.setTitle(R.string.app_name);
+//                d.setView(textView);
+//                d.show();
+//                return false;
+//            }
+//        });
+
     }
+
+    private void showAlertDialog(Context context) {
+        TextView textView = new TextView(context);
+        textView.setText("Please, check at least one language");
+        final AlertDialog.Builder d = new AlertDialog.Builder(context);
+        d.setTitle(R.string.app_name);
+        d.setView(textView);
+        d.show();
+    }
+
+
+    Preference.OnPreferenceChangeListener languageChangeListener  =
+        new Preference.OnPreferenceChangeListener() {
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            if(newValue == null) {
+                Log.v(TAG, "no languages selected");
+            } else {
+                try {
+                    Set<String> selectedLanguages = (HashSet<String>) newValue;
+                    if (selectedLanguages != null) {
+                        if(selectedLanguages.size() > 0) {
+                            CharSequence[] seq = selectedLanguages.toArray(new CharSequence[selectedLanguages.size()]);
+                            for (int ix = 0; ix < seq.length; ix++) {
+                                Log.v(TAG, "->->-> seq[" + ix + "] = " + seq[ix]);
+                            }
+                            return true;
+                        } else {
+                            Log.v(TAG, "no languages selected");
+                            showAlertDialog(getContext());
+                        }
+                    }
+                } catch (ClassCastException ignored) {
+                    Log.v(TAG, "xxxxxxxxxxxxxxxx" + ignored.getMessage());
+                }
+            }
+
+            return false;
+        }
+    };
+
 
 }
--- app/src/main/java/br/com/pearls/Settings/SettingsActivity.java
+++ app/src/main/java/br/com/pearls/Settings/SettingsActivity.java
@@ -1,10 +1,15 @@
 package br.com.pearls.Settings;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.preference.PreferenceFragmentCompat;
@@ -30,34 +35,42 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.settings_activity);
 
         mPearlsViewModel = new ViewModelProvider(this).get(PearlsViewModel.class);
-
         mPearlsViewModel.getmAllLanguages().observe(this, new Observer<List<Language>>() {
             @Override
             public void onChanged(List<Language> languages) {
-                List<CharSequence> entries = new ArrayList<>();
-                List<CharSequence> entryValues = new ArrayList<>();
-
-                for(int ix = 0; ix < languages.size(); ix++) {
-                    entries.add(languages.get(ix).getLanguage());
-                    entryValues.add(String.valueOf(languages.get(ix).getId()));
-                }
-
-                LANGUAGE_ENTRIES = new Bundle();
-                LANGUAGE_ENTRIES.putCharSequenceArray("entries", entries.toArray(new CharSequence[entries.size()]));
-                LANGUAGE_ENTRIES.putCharSequenceArray("entryValues", entryValues.toArray(new CharSequence[entryValues.size()]));
-
-                // create settings fragment ONLY AFTER having gotten languages from db
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.settings, new SettingsFragment())
-                        .commit();
-                ActionBar actionBar = getSupportActionBar();
-                if (actionBar != null) {
-                    actionBar.setDisplayHomeAsUpEnabled(true);
-                }
-
+                setLanguageEntries(languages);
             }
         });
+    }
+
+    private void setLanguageEntries(List<Language> languages) {
+        List<CharSequence> entries = new ArrayList<>();
+        List<CharSequence> entryValues = new ArrayList<>();
+
+        for(int ix = 0; ix < languages.size(); ix++) {
+            entries.add(languages.get(ix).getLanguage());
+            entryValues.add(String.valueOf(languages.get(ix).getId()));
+        }
+
+        LANGUAGE_ENTRIES = new Bundle();
+        LANGUAGE_ENTRIES.putCharSequenceArray("entries",
+                entries.toArray(new CharSequence[entries.size()]));
+        LANGUAGE_ENTRIES.putCharSequenceArray("entryValues",
+                entryValues.toArray(new CharSequence[entryValues.size()]));
+
+        // create settings fragment ONLY AFTER having gotten languages from db
+        createSettingsFragment();
+    }
+
+    private void createSettingsFragment() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings, new SettingsFragment())
+                .commit();
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 
     // LanguageMultiSelect obj calls this method for setting entries and entry values
@@ -71,5 +84,7 @@ public class SettingsActivity extends AppCompatActivity {
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
         }
+
     }
+
 }
